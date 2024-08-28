e3d77f8f8cd8ea3e6c0a8dc844dba126445c0625
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-28 14:26:56 +0100

    Add descriptive content and implement responsiveness

--- src/components/HomeMain.tsx
+++ src/components/HomeMain.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const HomeMain = () => {
     return (
         <div className="relative h-screen">
@@ -8,11 +10,21 @@ const HomeMain = () => {
                 />
             </video>
 
-            <div className="absolute top-1/2 -translate-y-1/2 md:px-20 px-5 text-white">
-                <h1 className="text-6xl font-bold">
-                    Blog your way <br /> round the world
+            <div className="lg:w-2/3 w-full absolute top-1/2 -translate-y-1/2 md:px-20 px-5 text-white flex flex-col md:items-start items-center md:text-left text-center space-y-5">
+                <h1 className="lg:text-6xl md:text-5xl text-4xl font-bold drop-shadow-lg">
+                    Journey the Globe One Blog at a Time
                 </h1>
-                <p className="pt-5">Share your stories with the world</p>
+                <p>
+                    Explore the world through your words. Share your journeys, inspire others, and
+                    connect with fellow travelers. Your adventure starts here—blog your way around
+                    the world.
+                </p>
+                <Link
+                    to="/blog"
+                    className="bg-white text-black px-5 py-2 hover:scale-105 transition-all duration-300"
+                >
+                    Start Blogging
+                </Link>
             </div>
         </div>
     );
