d86069171ff6d7e5316cdbe2d3c6a2bac4c80f23
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-28 14:27:25 +0100

    Import Home Blog

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,11 @@
-// https://videos.pexels.com/video-files/2257010/2257010-uhd_3840_2160_24fps.mp4
-
+import HomeBlog from "../components/HomeBlog";
 import HomeMain from "../components/HomeMain";
 
 const HomePage = () => {
     return (
         <div>
             <HomeMain />
+            <HomeBlog />
         </div>
     );
 };
