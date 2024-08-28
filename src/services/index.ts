31ffd30b4f53d1fcfafdb4ff92900a3dcdd1a9bd
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-28 15:18:02 +0100

    Get all categories

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 import { LoginData, AuthResponse, RegisterData } from "../types/auth";
+import { CategoryResponse } from "../types/categories";
 import $http from "./axiosConfig"
 
 export const LoginApi = async (loginData: LoginData): Promise<AuthResponse> => {
@@ -8,6 +9,13 @@ export const LoginApi = async (loginData: LoginData): Promise<AuthResponse> => {
 }
 export const RegisterApi = async (registerData: RegisterData): Promise<AuthResponse> => {
     const response = await $http.post(`/api/v1/users/signup`, registerData);
+
+    return response.data;
+}
+
+// Categories
+export const GetCategoriesApi = async (): Promise<CategoryResponse> => {
+    const response = await $http.get(`/api/v1/categories`);
 
     return response.data;
 }
