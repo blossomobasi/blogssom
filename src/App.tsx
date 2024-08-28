ef334a1b81004e3ad26346891311137f508a2f8e
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-28 14:25:14 +0100

    Configure React query and react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,32 @@ import BlogPage from "./pages/BlogPage";
 import AppLayout from "./components/AppLayout";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import { ToastContainer } from "react-toastify";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+    const queryClient = new QueryClient();
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<AppLayout />}>
-                    <Route index element={<HomePage />} />
-                    <Route path="/blog" element={<BlogPage />} />
-                </Route>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <ReactQueryDevtools initialIsOpen={false} />
 
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
+                <ToastContainer newestOnTop={true} pauseOnHover={true} autoClose={3000} draggable />
 
-                <Route path="*" element={<h1>Not Found</h1>} />
-            </Routes>
-        </BrowserRouter>
+                <Routes>
+                    <Route element={<AppLayout />}>
+                        <Route index element={<HomePage />} />
+                        <Route path="/blog" element={<BlogPage />} />
+                    </Route>
+
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+
+                    <Route path="*" element={<h1>Not Found</h1>} />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     );
 }
 
