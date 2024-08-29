cb09000d421ebf9876daa9007403411491d7595b
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-29 16:47:58 +0100

    Conditionally render signup and login button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,22 @@
 import { Link, NavLink } from "react-router-dom";
 import Logo from "./Logo";
+import { useUser } from "../hooks/useUser";
+import { CiLogout } from "react-icons/ci";
+import { useLogout } from "../hooks/useLogout";
+import clsx from "clsx";
 
 const NavBar = () => {
+    const { user } = useUser();
+    const { logout, isLoggingout } = useLogout();
+
     const navData = [
         { name: "Home", link: "/" },
-        { name: "Blog", link: "/blog" },
+        { name: "Blog", link: "/blogs" },
         { name: "Categories", link: "/category" }, // should have subcategories
         { name: "Gallery", link: "/gallery" },
     ];
     return (
-        <header className="flex justify-between items-center md:px-20 px-5 py-5 fixed w-full text-white z-50 backdrop-blur-md">
+        <header className="flex justify-between items-center md:px-20 px-5 py-5 fixed w-full text-white z-50 backdrop-blur-md bg-black/40">
             <Logo />
 
             <nav>
@@ -22,20 +29,47 @@ const NavBar = () => {
                 </ul>
             </nav>
 
-            <div className="flex space-x-3">
-                <Link
-                    to="/login"
-                    className="text-white border hover:bg-white hover:font-medium hover:text-black px-5 py-2 transition-colors duration-300"
-                >
-                    Log In
-                </Link>
-                <Link
-                    to="/register"
-                    className="bg-white border text-black hover:font-medium px-5 py-2"
-                >
-                    Sign Up
-                </Link>
-            </div>
+            {!user?.data.user ? (
+                <div className="flex space-x-3">
+                    <Link
+                        to="/login"
+                        className="text-white border hover:bg-white hover:font-medium hover:text-black px-5 py-2 transition-colors duration-300"
+                    >
+                        Log In
+                    </Link>
+                    <Link
+                        to="/register"
+                        className="bg-white border text-black hover:font-medium px-5 py-2"
+                    >
+                        Sign Up
+                    </Link>
+                </div>
+            ) : (
+                <div className="flex space-x-3">
+                    <figure className="flex items-center space-x-2">
+                        <img
+                            src={user?.data.user.avatar}
+                            alt="avatar"
+                            className="w-10 h-10 rounded-full"
+                        />
+                        <figcaption>
+                            {user?.data.user.firstName} {user?.data.user.lastName}
+                        </figcaption>
+                    </figure>
+
+                    <span
+                        onClick={() => logout()}
+                        className={clsx(
+                            "bg-white border text-black hover:font-medium px-5 py-2 cursor-pointer",
+                            {
+                                "opacity-50 cursor-not-allowed": isLoggingout,
+                            }
+                        )}
+                    >
+                        <CiLogout size={23} />
+                    </span>
+                </div>
+            )}
         </header>
     );
 };
