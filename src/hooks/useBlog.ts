b671ccc815ee9b985661a861aeb8312aaea05016
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-29 19:49:00 +0100

    Add get blog api hook

--- src/hooks/useBlog.ts
+++ src/hooks/useBlog.ts
@@ -1,11 +1,18 @@
 import { useQuery } from "@tanstack/react-query"
-import { GetBlogsApi } from "../services"
+import { GetBlogApi, GetBlogsApi } from "../services"
 
-export const useBlog = (categoryId?: string) => {
+export const useBlog = (categoryId?: string, blogId?: string) => {
     const { data: blogs, isLoading: isFetchingBlogs, error: blogError } = useQuery({
         queryKey: ["blogs", categoryId],
         queryFn: () => GetBlogsApi(categoryId),
     })
 
-    return { blogs, isFetchingBlogs, blogError }
+    const { data: blog, isLoading: isFetchingBlog, error } = useQuery({
+        queryKey: ["blog", blogId],
+        queryFn: () => GetBlogApi(blogId as string),
+    })
+
+
+
+    return { blogs, blog, isFetchingBlog, isFetchingBlogs, blogError, error }
 }
