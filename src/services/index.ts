d10ea72d4dd019511f8028160a710e644c77483a
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-29 16:50:17 +0100

    Add logout api, get user api, and get category api

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
-import { LoginData, AuthResponse, RegisterData } from "../types/auth";
+import { LoginData, AuthResponse, RegisterData, UserResponse } from "../types/auth";
 import { BlogResponse } from "../types/blog";
 import { CategoryResponse } from "../types/categories";
 import $http from "./axiosConfig"
@@ -8,15 +8,30 @@ export const LoginApi = async (loginData: LoginData): Promise<AuthResponse> => {
 
     return response.data;
 }
+export const LogoutApi = async (): Promise<void> => await $http.get(`/api/v1/users/logout`);
+
+
 export const RegisterApi = async (registerData: RegisterData): Promise<AuthResponse> => {
     const response = await $http.post(`/api/v1/users/signup`, registerData);
 
     return response.data;
 }
 
+// Get User
+export const GetUserApi = async (): Promise<UserResponse> => {
+    const response = await $http.get(`/api/v1/users/me`);
+
+    return response.data
+}
+
 // Categories
 export const GetCategoriesApi = async (): Promise<CategoryResponse> => {
     const response = await $http.get(`/api/v1/categories`);
+
+    return response.data;
+}
+export const GetCategoryApi = async (categoryId: string): Promise<CategoryResponse> => {
+    const response = await $http.get(`/api/v1/categories/${categoryId}`);
 
     return response.data;
 }
