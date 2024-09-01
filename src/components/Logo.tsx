85cf6876d50a4f7eeebfea9dc3f5104b304fb2d9
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-01 16:29:02 +0100

    Add size option

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,15 @@
+import clsx from "clsx";
 import { Link } from "react-router-dom";
 
-const Logo = () => {
+const Logo = ({ size = "lg" }: { size?: "lg" | "sm" }) => {
     return (
-        <Link to="/" className="xl font-semibold">
+        <Link
+            to="/"
+            className={clsx("xl font-semibold", {
+                "text-2xl": size === "lg",
+                "text-xl": size === "sm",
+            })}
+        >
             <span>✈</span>Blogssom <span>🧳</span>
         </Link>
     );
