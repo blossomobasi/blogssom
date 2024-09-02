999dc8f4797631afab0e66c4d1ecd22e15b829ec
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-02 23:26:01 +0100

    Create trucateText function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,11 @@ export const formatDate = (date: string) => {
     const d = new Date(date);
     return d.toDateString().split(" ").slice(1, 4).join(" ");
 };
+
+export const truncateText = (text: string, wordLimit: number) => {
+    const words = text.split(' ');
+    if (words.length > wordLimit) {
+        return words.slice(0, wordLimit).join(' ') + '...';
+    }
+    return text;
+};
