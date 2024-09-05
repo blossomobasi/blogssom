6b5565a1f56467ed4f0010f9016652dad308a942
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-05 12:15:41 +0100

    Update: Implement adding of comment on a blog

--- src/components/ShowComment.tsx
+++ src/components/ShowComment.tsx
@@ -5,7 +5,8 @@ import clsx from "clsx";
 import { IoMdClose } from "react-icons/io";
 import { useComment } from "../hooks/useComment";
 import MiniSpinner from "../ui/MiniSpinner";
-import { FaRegHeart } from "react-icons/fa";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useState } from "react";
 
 type ShowCommentProps = {
     onShowModal: (id: string) => void;
@@ -14,8 +15,42 @@ type ShowCommentProps = {
 
 const ShowComment = ({ onShowModal, blog }: ShowCommentProps) => {
     const { user } = useUser();
-    const { isCommentsLoading, comments } = useComment(blog._id);
+    const {
+        isCommentsLoading,
+        isCreatingComment,
+        comments,
+        createComment,
+        likeComment,
+        unlikeComment,
+    } = useComment(blog._id);
     const navigate = useNavigate();
+    const [comment, setComment] = useState("");
+
+    const hasAlreadyLikedComment = (commentId: string) => {
+        const comment = comments?.data.comments.find((comment) => comment._id === commentId);
+        if (!comment || !user) return false;
+
+        return comment?.likes.includes(user?.data.user._id);
+    };
+
+    function handleLike(commentId: string) {
+        if (!user) {
+            navigate("/login");
+        }
+        likeComment(commentId);
+    }
+
+    function handleUnlike(commentId: string) {
+        unlikeComment(commentId);
+    }
+
+    function handleAddComment(e: React.FormEvent) {
+        e.preventDefault();
+        if (!comment) return;
+
+        createComment(comment);
+        setComment("");
+    }
 
     return (
         <div className="flex justify-center">
@@ -33,7 +68,7 @@ const ShowComment = ({ onShowModal, blog }: ShowCommentProps) => {
                     }
                 )}
             >
-                <div className="flex justify-between items-center space-x-5 pt-6">
+                <div className="flex justify-between md:flex-row flex-col items-start gap-x-5 pt-6">
                     <button
                         onClick={() => onShowModal("")}
                         className="text-white absolute top-0 right-0 bg-red-500 p-1"
@@ -44,12 +79,31 @@ const ShowComment = ({ onShowModal, blog }: ShowCommentProps) => {
                     <h1 className="text-lg font-semibold">Comments</h1>
 
                     {user && (
-                        <button
-                            className="bg-blue-500 text-white px-3 py-1 rounded-lg"
-                            onClick={() => navigate(`/blogs/${blog._id}/comment`)}
+                        <form
+                            className="flex md:flex-row flex-col items-start gap-3"
+                            onSubmit={(e) => handleAddComment(e)}
                         >
-                            Add Comment
-                        </button>
+                            <textarea
+                                cols={30}
+                                rows={3}
+                                placeholder="Write a comment"
+                                autoFocus
+                                value={comment}
+                                onChange={(e) => setComment(e.target.value)}
+                                className="border border-gray-500 p-1 rounded-md focus-within:outline-none w-full"
+                            />
+
+                            <button
+                                className={clsx(
+                                    "bg-black text-white py-2 px-3 rounded-md flex items-center gap-x-3 whitespace-nowrap",
+                                    {
+                                        "cursor-not-allowed opacity-60": isCreatingComment,
+                                    }
+                                )}
+                            >
+                                Add Comment {isCreatingComment && <MiniSpinner />}
+                            </button>
+                        </form>
                     )}
                 </div>
                 {isCommentsLoading ? (
@@ -70,7 +124,7 @@ const ShowComment = ({ onShowModal, blog }: ShowCommentProps) => {
                                         <img
                                             src={comment.user.avatar}
                                             alt={comment.user.firstName}
-                                            className="w-10 h-10 rounded-full"
+                                            className="w-10 h-10 rounded-full self-start"
                                         />
                                         <div>
                                             <p className="text-gray-600 font-medium text-lg">
@@ -80,8 +134,21 @@ const ShowComment = ({ onShowModal, blog }: ShowCommentProps) => {
                                                 {comment.content}
                                             </p>
 
-                                            <span className="text-gray-500 mt-5">
-                                                <FaRegHeart />
+                                            <span className="text-gray-500 mt-2 flex items-center gap-2 font-medium text-lg">
+                                                {!hasAlreadyLikedComment(comment._id) ? (
+                                                    <FaRegHeart
+                                                        size={20}
+                                                        onClick={() => handleLike(comment._id)}
+                                                        className="cursor-pointer"
+                                                    />
+                                                ) : (
+                                                    <FaHeart
+                                                        size={20}
+                                                        onClick={() => handleUnlike(comment._id)}
+                                                        className="text-red-500 cursor-pointer"
+                                                    />
+                                                )}
+                                                {comment.likes.length}
                                             </span>
                                         </div>
                                     </div>
