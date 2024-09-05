c61bdd7865098952ab03751a91bd6ed825493565
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-05 12:15:14 +0100

    Implement protect route

---
+++ src/components/ProtectRoute.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { useUser } from "../hooks/useUser";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../ui/Spinner";
+import { toast } from "react-toastify";
+import { AxiosError } from "axios";
+
+const ProtectRoute = ({ children }: { children: React.ReactNode }) => {
+    const { user, isLoading, error } = useUser();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isLoading && !user) navigate("/login");
+    }, [user, isLoading, navigate]);
+
+    if (isLoading) return <Spinner />;
+
+    if (error) {
+        const err: AxiosError = error;
+        const errorMessage = (err.response?.data as { message: string }).message;
+
+        toast.error(errorMessage);
+    }
+
+    return <div>{children}</div>;
+};
+
+export default ProtectRoute;
