96a33bcb72ca5e4ba2fee931cc19c5833af0108c
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-12 17:03:35 +0100

    Add hook for getting a users blog

--- src/hooks/useBlog.ts
+++ src/hooks/useBlog.ts
@@ -1,16 +1,43 @@
-import { useQuery } from "@tanstack/react-query"
-import { GetBlogApi, GetBlogsApi } from "../services"
+import { useQuery } from "@tanstack/react-query";
+import { GetBlogApi, GetBlogsApi, GetMyBlogsApi } from "../services";
 
 export const useBlog = (categoryId?: string, blogId?: string) => {
-    const { data: blogs, isLoading: isFetchingBlogs, error: blogError } = useQuery({
+    const {
+        data: blogs,
+        isLoading: isFetchingBlogs,
+        error: blogError,
+    } = useQuery({
         queryKey: ["blogs", categoryId],
         queryFn: () => GetBlogsApi(categoryId),
-    })
+    });
 
-    const { data: blog, isLoading: isFetchingBlog, error } = useQuery({
+    const {
+        data: blog,
+        isLoading: isFetchingBlog,
+        error,
+    } = useQuery({
         queryKey: ["blog", blogId],
         queryFn: () => GetBlogApi(blogId as string),
-    })
+    });
 
-    return { blogs, blog, isFetchingBlog, isFetchingBlogs, blogError, error }
-}
+    const {
+        data: myBlogs,
+        isLoading: isFetchingMyBlog,
+        error: myBlogError,
+    } = useQuery({
+        queryKey: ["blog"],
+        queryFn: GetMyBlogsApi,
+    });
+
+    return {
+        blogs,
+        myBlogs,
+        blog,
+        isFetchingBlog,
+        isFetchingBlogs,
+        isFetchingMyBlog,
+        blogError,
+        myBlogError,
+        error,
+    };
+};
