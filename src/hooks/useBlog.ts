eb875a30f1d074ea171123c875bb3e5ccddfd587
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-12 19:59:03 +0100

    Add hook for deleting a product

--- src/hooks/useBlog.ts
+++ src/hooks/useBlog.ts
@@ -1,7 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
-import { GetBlogApi, GetBlogsApi, GetMyBlogsApi } from "../services";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { DeleteBlogApi, GetBlogApi, GetBlogsApi, GetMyBlogsApi } from "../services";
+import { toast } from "react-toastify";
+import { AxiosError } from "axios";
 
 export const useBlog = (categoryId?: string, blogId?: string) => {
+    const queryClient = useQueryClient();
     const {
         data: blogs,
         isLoading: isFetchingBlogs,
@@ -29,10 +32,24 @@ export const useBlog = (categoryId?: string, blogId?: string) => {
         queryFn: GetMyBlogsApi,
     });
 
+    const { mutate: deleteBlog, isPending: isDeletingBlog } = useMutation({
+        mutationFn: DeleteBlogApi,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["blog", blogId] });
+            toast.success("Blog deleted successfully");
+        },
+        onError: (err: AxiosError) => {
+            const errorMessage = (err.response?.data as { message: string }).message;
+            toast.error(errorMessage);
+        },
+    });
+
     return {
         blogs,
         myBlogs,
         blog,
+        deleteBlog,
+        isDeletingBlog,
         isFetchingBlog,
         isFetchingBlogs,
         isFetchingMyBlog,
