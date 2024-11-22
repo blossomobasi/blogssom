0dad634b61fe55637db1ad34456595dee9c3337a
Blossom Obasi <blossomobasi2@gmail.com>
2024-11-22 10:40:43 +0100

    Refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -15,47 +15,47 @@ import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
 
 function App() {
-    const queryClient = new QueryClient();
-    return (
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <ReactQueryDevtools initialIsOpen={false} />
+	const queryClient = new QueryClient();
+	return (
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<ReactQueryDevtools initialIsOpen={false} />
 
-                <ToastContainer newestOnTop={true} pauseOnHover={true} autoClose={3000} draggable />
+				<ToastContainer newestOnTop={true} pauseOnHover={true} autoClose={3000} draggable />
 
-                <Routes>
-                    <Route element={<AppLayout />}>
-                        <Route index element={<HomePage />} />
-                        <Route path="/blogs" element={<BlogPage />} />
-                        <Route path="/blogs/:blogId" element={<BlogIdPage />} />
-                        <Route
-                            path="/my-blogs"
-                            element={
-                                <ProtectRoute>
-                                    <MyBlogsPage />
-                                </ProtectRoute>
-                            }
-                        />
-                        <Route
-                            path="/blogs/create"
-                            element={
-                                <ProtectRoute>
-                                    <CreateBlogPage />
-                                </ProtectRoute>
-                            }
-                        />
-                    </Route>
+				<Routes>
+					<Route element={<AppLayout />}>
+						<Route index element={<HomePage />} />
+						<Route path="/blogs" element={<BlogPage />} />
+						<Route path="/blogs/:blogId" element={<BlogIdPage />} />
+						<Route
+							path="/my-blogs"
+							element={
+								<ProtectRoute>
+									<MyBlogsPage />
+								</ProtectRoute>
+							}
+						/>
+						<Route
+							path="/blogs/create"
+							element={
+								<ProtectRoute>
+									<CreateBlogPage />
+								</ProtectRoute>
+							}
+						/>
+					</Route>
 
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
-                    <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-                    <Route path="/reset-password" element={<ResetPasswordPage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/register" element={<RegisterPage />} />
+					<Route path="/forgot-password" element={<ForgotPasswordPage />} />
+					<Route path="/reset-password" element={<ResetPasswordPage />} />
 
-                    <Route path="*" element={<h1>Not Found</h1>} />
-                </Routes>
-            </BrowserRouter>
-        </QueryClientProvider>
-    );
+					<Route path="*" element={<h1>Not Found</h1>} />
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
+	);
 }
 
 export default App;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -13,110 +13,96 @@ import { FaRegEyeSlash } from "react-icons/fa";
 import { MdOutlineRemoveRedEye } from "react-icons/md";
 
 const LoginPage = () => {
-    const [showPassword, setShowPassword] = useState(true);
-    const { register, handleSubmit, formState } = useForm<LoginData>();
-    const { errors } = formState;
-    const navigate = useNavigate();
+	const [showPassword, setShowPassword] = useState(true);
+	const { register, handleSubmit, formState } = useForm<LoginData>();
+	const { errors } = formState;
+	const navigate = useNavigate();
 
-    const { mutate: Login, isPending: isLoggingIn } = useMutation({
-        mutationFn: LoginApi,
-        onSuccess: () => {
-            toast.success("Login successful");
-            navigate("/");
-        },
-        onError: (err: AxiosError) => {
-            const errorMessage = (err.response?.data as { message: string }).message;
-            toast.error(errorMessage);
-        },
-    });
+	const { mutate: Login, isPending: isLoggingIn } = useMutation({
+		mutationFn: LoginApi,
+		onSuccess: () => {
+			toast.success("Login successful");
+			navigate("/");
+		},
+		onError: (err: AxiosError) => {
+			const errorMessage = (err.response?.data as { message: string }).message;
+			toast.error(errorMessage);
+		},
+	});
 
-    const onSubmit = (data: LoginData) => {
-        Login(data);
-    };
+	const onSubmit = (data: LoginData) => {
+		Login(data);
+	};
 
-    return (
-        <AuthLayout>
-            <div className="w-[30rem]">
-                <div>
-                    <h1 className="text-4xl mb-5 font-bold">Login</h1>
-                    <p className="mb-8 text-gray-600">
-                        Don't have an account yet?{" "}
-                        <Link to="/register" className="text-black hover:underline font-semibold">
-                            Register
-                        </Link>
-                    </p>
-                </div>
+	return (
+		<AuthLayout>
+			<div className="w-[30rem]">
+				<div>
+					<h1 className="text-4xl mb-5 font-bold">Login</h1>
+					<p className="mb-8 text-gray-600">
+						Don't have an account yet?{" "}
+						<Link to="/register" className="text-black hover:underline font-semibold">
+							Register
+						</Link>
+					</p>
+				</div>
 
-                <form className="flex flex-col space-y-3" onSubmit={handleSubmit(onSubmit)}>
-                    <input
-                        type="email"
-                        id="email"
-                        {...register("email", {
-                            required: "Email is required",
-                            pattern: /^\S+@\S+$/i,
-                        })}
-                        className="border px-2 py-3 w-full placeholder:text-gray-600 focus-within:border-black focus-within:outline-none"
-                        placeholder="Email"
-                    />
-                    {errors.email && (
-                        <span className="text-red-500 text-sm">{errors.email.message}</span>
-                    )}
-                    <div className="border flex items-center focus-within:border-black">
-                        <input
-                            type={showPassword ? "password" : "text"}
-                            id="password"
-                            {...register("password", {
-                                required: "Password is required",
-                                minLength: {
-                                    value: 8,
-                                    message: "Password must be at least 8 characters",
-                                },
-                            })}
-                            className="px-2 py-3 w-full placeholder:text-gray-600 focus-within:border-black focus-within:outline-none"
-                            placeholder="Password"
-                        />
-                        <span
-                            onClick={() => setShowPassword((show) => !show)}
-                            className="px-3 cursor-pointer"
-                        >
-                            {showPassword ? (
-                                <FaRegEyeSlash size={25} />
-                            ) : (
-                                <MdOutlineRemoveRedEye size={25} />
-                            )}
-                        </span>
-                    </div>
-                    {errors.password && (
-                        <span className="text-red-500 text-sm">{errors.password.message}</span>
-                    )}
+				<form className="flex flex-col space-y-3" onSubmit={handleSubmit(onSubmit)}>
+					<input
+						type="email"
+						id="email"
+						{...register("email", {
+							required: "Email is required",
+							pattern: /^\S+@\S+$/i,
+						})}
+						className="border px-2 py-3 w-full placeholder:text-gray-600 focus-within:border-black focus-within:outline-none"
+						placeholder="Email"
+					/>
+					{errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
+					<div className="border flex items-center focus-within:border-black">
+						<input
+							type={showPassword ? "password" : "text"}
+							id="password"
+							{...register("password", {
+								required: "Password is required",
+								minLength: {
+									value: 8,
+									message: "Password must be at least 8 characters",
+								},
+							})}
+							className="px-2 py-3 w-full placeholder:text-gray-600 focus-within:border-black focus-within:outline-none"
+							placeholder="Password"
+						/>
+						<span onClick={() => setShowPassword((show) => !show)} className="px-3 cursor-pointer">
+							{showPassword ? <FaRegEyeSlash size={25} /> : <MdOutlineRemoveRedEye size={25} />}
+						</span>
+					</div>
+					{errors.password && <span className="text-red-500 text-sm">{errors.password.message}</span>}
 
-                    <div className="flex items-center justify-between py-3">
-                        <div className="flex space-x-3">
-                            <input type="checkbox" id="rememberMe" />
-                            <label htmlFor="rememberMe" className="text-gray=-600">
-                                Remember me
-                            </label>
-                        </div>
+					<div className="flex items-center justify-between py-3">
+						<div className="flex space-x-3">
+							<input type="checkbox" id="rememberMe" />
+							<label htmlFor="rememberMe" className="text-gray=-600">
+								Remember me
+							</label>
+						</div>
 
-                        <Link to="/forgot-password" className="font-semibold">
-                            Forgot Password?
-                        </Link>
-                    </div>
+						<Link to="/forgot-password" className="font-semibold">
+							Forgot Password?
+						</Link>
+					</div>
 
-                    <button
-                        className={clsx(
-                            "bg-black text-white py-3 font-semibold flex items-center justify-center",
-                            {
-                                "cursor-not-allowed opacity-60": isLoggingIn,
-                            }
-                        )}
-                    >
-                        {isLoggingIn ? <MiniSpinner /> : "Login"}
-                    </button>
-                </form>
-            </div>
-        </AuthLayout>
-    );
+					<button
+						className={clsx("bg-black text-white py-3 font-semibold flex items-center justify-center", {
+							"cursor-not-allowed opacity-60": isLoggingIn,
+						})}
+					>
+						{isLoggingIn ? <MiniSpinner /> : "Login"}
+					</button>
+				</form>
+			</div>
+		</AuthLayout>
+	);
 };
 
 export default LoginPage;
